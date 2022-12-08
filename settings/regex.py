5e7eceaefa155a988ff979461c81918d6aaa5c7e
LucaBru <lucabrugnera01@gmail.com>
2022-12-08 20:19:16 +0100

    Add some rules for official documents
    Add controlls about directories's tree and src, src/sections files
    CEA-43 #time 45m

--- settings/regex.py
+++ settings/regex.py
@@ -6,13 +6,6 @@ def checkLatexFilesCorrectness():
     # file's extension which are checks
     extensions = ['.txt', '.tex', '.py', '.pdf']
 
-    # final / official document which version name are checks
-    official_documents_name = [
-        '*/Analisi dei requisiti/*.pdf',
-        '*/Norme di progetto/*.pdf',
-        '*/Piano di progetto/*.pdf',
-    ]
-
     wd = Path(Path.cwd().parent) 
 
     # all files name in the wd that have extensions extension are controlled
@@ -24,9 +17,49 @@ def checkLatexFilesCorrectness():
         # other check like structure... 
     
     
+    
+    official_docs_dir = [
+        '*/Analisi dei requisiti/',
+        '*/Norme di progetto/',
+        '*/Piano di progetto/',
+    ]
+
+    for dir_name in official_docs_dir:
+        dir = Path(dir_name)
+
+        # directories tree is '*/dir_name/src/sections/'
+        if not (dir/'src/sections/').exists():
+            print ('Directories tree not correct ', dir)
+        else:
+            # dir has only one .pdf file
+            if len(list(dir.glob('*.pdf'))) != 1:
+                print ('More than one pdf file in ', dir)
+            # TODO parte only
+
+            # '*/dir_name/src/' has only one .tex file
+            if len(list((dir/'src').glob('*.tex'))) != 1:
+                print ('More than one tex file in ', dir/'src')
+            # TODO parte only
+            
+            # '*/dir_name/src/sections/' has:
+            #   - only tex files TODO
+            #   - styles.tex
+            #   - packages.tex
+            #   - title_page.tex
+            
+            sections_path = dir/'src/sections/'
+            necessary_files_sections_dir = [
+                'styles.tex',
+                'packages',
+                'title_page.tex',
+            ]
+
+
+
+
+
 
     #if i am here ==> no failure ==> files are corrects
-    return True
 
     
 checkLatexFilesCorrectness()
