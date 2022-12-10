ee273776ba61ec2ccf5fd0b27611527ae2bdbace
LucaBru <lucabrugnera01@gmail.com>
2022-12-10 14:10:15 +0100

    Improve regex.py

--- settings/regex.py
+++ settings/regex.py
@@ -1,12 +1,13 @@
 from pathlib import Path
-from settings.expression_file import *
+import collections
+from expression_file import *
 
 def checkLatexFilesCorrectness():
 
     # file's extension which are checks
     extensions = ['.txt', '.tex', '.py', '.pdf']
 
-    wd = Path(Path.cwd().parent) 
+    wd = Path('.').absolute()
 
     # all files name in the wd that have extensions extension are controlled
     files = [file for file in wd.rglob('*') if file.suffix in extensions]
@@ -19,27 +20,41 @@ def checkLatexFilesCorrectness():
     
     
     official_docs_dir = [
-        '*/Analisi dei requisiti/',
-        '*/Norme di progetto/',
-        '*/Piano di progetto/',
+        'Analisi dei requisiti/',
+        'Norme di progetto/',
+        'Piano di progetto/',
     ]
 
     for dir_name in official_docs_dir:
-        dir = Path(dir_name)
-
+        dir = wd/dir_name
         # directories tree is '*/dir_name/src/sections/'
         if not (dir/'src/sections/').exists():
             print ('Directories tree not correct ', dir)
         else:
-            # dir has only one .pdf file
-            if len(list(dir.glob('*.pdf'))) != 1:
-                print ('More than one pdf file in ', dir)
-            # TODO parte only
+
+            # official doc dir has only one .pdf file
+            dir_files = collections.Counter(dir.glob('*'))
+            if len(dir_files) == 1:
+                if dir_files.get('.pdf', False):
+                    print('File must be pdf in', dir)
+                
+            else:
+                if len(dir_files) > 1:
+                    print('Only one pdf file is admit in ', dir)
+                else:
+                    print(dir, 'must have a pdf file')
+        
 
             # '*/dir_name/src/' has only one .tex file
-            if len(list((dir/'src').glob('*.tex'))) != 1:
-                print ('More than one tex file in ', dir/'src')
-            # TODO parte only
+            src_files = collections.Counter((dir/'src').glob('*.*'))
+            if len(src_files) == 1:
+                if src_files.get('.tex', False):
+                    print('File must be latex in ', dir/'src/')
+            else:
+                if len(src_files) > 1:
+                    print('Only one latex file is admit in ', dir/'src/')
+                else:
+                    print(dir/'src/', 'must have a latex file')
             
             # '*/dir_name/src/sections/' has:
             #   - only tex files TODO
