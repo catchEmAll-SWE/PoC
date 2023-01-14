09c46e2974191ad757eb53ba8cfc92ef37c3a57d
LucaBru <lucabrugnera01@gmail.com>
2023-01-14 11:11:32 +0100

    Update main.py with a list of works (TODO)
    CEA-64 #time 15m

--- Controlli automatici/Correttezza glossario/main.py
+++ Controlli automatici/Correttezza glossario/main.py
@@ -2,10 +2,12 @@ import re
 from pathlib import Path
 
 # TODO
-# ricerca delle parole all'interno del glossario
-# per ogni parola controllo se nei documenti ufficiali è presente \textsubscript{G} per ogni istanza di essa
+# fare il merge del main con il glossario in questo ramo
+# ritornare una lista di errori importando il modulo print_error
+# aggiungere lo script al workflow di github con trigger modifiche sul documento Glossario/src/sections/glossario.tex (così non si runna ogni volta che si fa un push inutilmente)
 
 def checkGlossaryWordPresenceInOfficialDocs(word: str) -> bool:
+    result = True
     official_docs_dirs = ['Analisi dei requisiti/', 'Norme di progetto/', 'Piano di progetto/', 'Piano di qualifica/']
     official_dirs = [Path('.')/dir_name for dir_name in official_docs_dirs]
     for dir in official_dirs:
@@ -14,8 +16,8 @@ def checkGlossaryWordPresenceInOfficialDocs(word: str) -> bool:
                 if file.suffix == '.tex':
                     file_text = file.read_text()
                     if re.search(r'word(?!\\textsubscript{\S*G\S*})', file_text):
-                        return False
-    return True
+                        result = False
+    return result
 
 #POST: return True if word is present in official docs, False and print files where it is not present otherwise
 
