1e7d90ac46a58762e00bf631974107173d411946
SinicatoNicola <sinickinfoutili@gmail.com>
2023-01-19 21:32:04 +0100

    "Add classes and function for the poc" "#time 2h"

---
+++ POC/CAPTCHA/classi/captcha.py
@@ -0,0 +1,32 @@
+import re
+import pandas as pd
+
+class Captcha:
+    def __init__(self, immaginijson, classi, classetask):
+        self.classetask = classetask
+        sceltaDelleImmagini(immaginijson, classi)
+        hashCaptcha()
+        
+    def sceltaDelleImmagini(self,immaginijson, classi):
+        immaginiprese = list()
+        lista_id_sicure, lista_id_nonsicure = parse_file(immaginijson,classi)
+            
+                
+                
+                
+            
+            
+        self.immagini = list()
+    
+    
+            
+            
+    def parse_line()
+    
+    def hashCaptcha(self):
+    
+    def getClasseTask(self):
+        return classetask
+    
+    def get
+    
---
+++ POC/CAPTCHA/classi/classe.py
@@ -0,0 +1,25 @@
+class Classe:
+    def __init__(self,nome):
+        self.nome = nome
+    
+    def setTotale(self, totale):
+        self.totale = totale
+    
+    def setSicure(self, sicure):
+        self.sicure = sicure
+        
+    def setNonSicure(self, nonsicure):
+        self.nonsicure = nonsicure
+        
+    def getNome(self):
+        return self.nome
+    
+    def getTotale(self):
+        return self.totale
+    
+    def getSicure(self):
+        return self.sicure
+    
+    def getNonSicure(self):
+        return self.nonsicure
+    
