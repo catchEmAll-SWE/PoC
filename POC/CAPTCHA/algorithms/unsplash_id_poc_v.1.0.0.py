4faca73e8813db86dc3759509745476a906b68ae
Matteo Stocco <mstocco11@gmail.com>
2023-02-06 22:27:42 +0100

    modified unsplash downloader script
    CEA-67 #time 2h

--- POC/CAPTCHA/algorithms/unsplash_id_poc_v.1.0.0.py
+++ POC/CAPTCHA/algorithms/unsplash_id_poc_v.1.0.0.py
@@ -1,19 +1,36 @@
 import requests as rq
 import json
 import os
+import mysql.connector
+from time import sleep
+import cv2 as cv
+import urllib.request
+import numpy as np
+
 
 ACCESS_KEY = "1CoGBz63unkif-f4iBuOzL4g6o2oP-OMmFoMs7quj24"
-valuableWord = "ship"
-
-
-os.chdir("../File_Json")
-
+valuableWord = ["ship", "car", "bird", "bottle", "laptop"]
+db = mysql.connector.connect(
+    host ="localhost",
+    user ="root",
+    passwd = "root-1234",
+    database = "captcha_db"
+)
+mycursor = db.cursor()
+dir = './immagini/' + 'img.jpg'
 for i in range(5):
     r = rq.get(
-          'https://api.unsplash.com/photos/random/?w=200&h=200&query={0}&client_id={1}'.format(valuableWord, ACCESS_KEY))
+          'https://api.unsplash.com/photos/random/?w=200&h=200&query={0}&client_id={1}'.format(valuableWord[i], ACCESS_KEY),allow_redirects=True)
     jsonContent = json.loads(r.content)
-    image = {"id": jsonContent["id"], "class": valuableWord, "reliability": 0}
-    json_image = json.dumps(image, indent=3)
-    with open("images.json", "a") as f:
-        f.write(json_image)
-    f.close
+    mycursor.execute("INSERT INTO immagine (idImmagine,classeImmagine,sicurezzaImmagine) VALUES(%s,%s,%s)", (jsonContent["id"],valuableWord[i],0))
+    db.commit()
+    dir = './immagini/' + jsonContent["id"] + '.jpg'
+    image = rq.get(jsonContent["urls"]["regular"], allow_redirects=True)
+    open(dir, 'wb').write(image.content)
+    raw_image = cv.imread(dir)
+    gray = cv.cvtColor(raw_image, cv.COLOR_BGR2GRAY)
+    bilFilter = cv.bilateralFilter(gray, 11, 17, 17)
+    canny = cv.Canny(bilFilter, 20, 200)
+    cv.imwrite(dir, canny)
+
+
--- POC/CAPTCHA/pagina login/index.php
+++ POC/CAPTCHA/pagina login/index.php
@@ -7,6 +7,20 @@
     <title>Login Page</title>
 </head>
 <body>
+    <?php
+    $dbconnection = mysqli_connect("localhost","root","root-1234","captcha_db");
+    if(!$dbconnection)
+    die("connessione non riuscita".mysqli_connect_error());
+    else
+    echo 'connessione riuscita';
+    $result = mysqli_query($dbconnection, "SELECT idImmagine FROM captcha_db.immagine");
+    $links = [];
+    $counter = 0;
+    while($row = mysqli_fetch_array($result)){
+        $links[$counter] = "https://source.unsplash.com/" . $row["idImmagine"];
+        $counter = $counter+1;
+    }
+    ?>
     <h1>
         <span lang="en">Login</span>
     </h1>
@@ -14,47 +28,47 @@
         <div class="captcha-grid">
             <div class="item">
                 <input type="checkbox" id="img1" />
-                <label for="img1"><img src="./prova2.png" width=100% 
+                <label for="img1"><img src="<?php echo $links[0]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img2" />
-                <label for="img2"><img src="./prova2.png" width=100% 
+                <label for="img2"><img src="<?php echo $links[1]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img3" />
-                <label for="img3"><img src="./prova2.png" width=100% 
+                <label for="img3"><img src="<?php echo $links[2]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img4" />
-                <label for="img4"><img src="./prova2.png" width=100% 
+                <label for="img4"><img src="<?php echo $links[3]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img5" />
-                <label for="img5"><img src="./prova2.png" width=100% 
+                <label for="img5"><img src="<?php echo $links[4]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img6" />
-                <label for="img6"><img src="./prova2.png" width=100% 
+                <label for="img6"><img src="<?php echo $links[5]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img7" />
-                <label for="img7"><img src="./prova2.png" width=100% 
+                <label for="img7"><img src="<?php echo $links[6]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img8" />
-                <label for="img8"><img src="./prova2.png" width=100% 
+                <label for="img8"><img src="<?php echo $links[7]?>" width=100% 
                     height=100%></label>
             </div>
             <div class="item">
                 <input type="checkbox" id="img9" />
-                <label for="img9"><img src="./prova2.png" width=100% 
+                <label for="img9"><img src="<?php echo $links[8]?>" width=100% 
                     height=100%></label>
             </div>
         </div>
