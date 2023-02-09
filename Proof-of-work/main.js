cb01f6d08c584368baabd2c3f2de8d25ba40c600
luca <lucabrugnera01@gmail.com>
2023-02-09 15:18:16 +0100

    update

--- Proof-of-work/main.js
+++ Proof-of-work/main.js
@@ -3,8 +3,10 @@ function calcHash(){
     difficulty = "000"
     for (let i=0; i<10; ++i){
         hashcode = crypto.subtle.digest('SHA-256', staticText+str(i));
-        if (hashcode.startsWith(difficulty))
-            return true;
+        if (hashcode.startsWith(difficulty)){
+            document.getElementById('nonce').value = hashcode
+            i = 10
+        }
     }
     return false
 }
