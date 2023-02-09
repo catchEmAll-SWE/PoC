b2ffde605e1a39dc0dd25c006e7b904cc9eacefa
luca <lucabrugnera01@gmail.com>
2023-02-09 15:47:39 +0100

    Update

--- Proof-of-work/validator.php
+++ Proof-of-work/validator.php
@@ -3,8 +3,14 @@ $username = $_POST['name'];
 $passwd = $_POST['passwdord'];
 $nonce_found = $_POST['nonce'];
 $difficulty = $_POST['difficulty'];
+$pageHTML = file_get_contents('main.html');
+$result = "";
 
 $hashcode = hash("sha256", $username.$passwd.$nonce_found);
-
-
+if (str_starts_with($hashcode, $difficulty))
+    $result = "Work done";
+else
+    $result = "Do your work";
+    
+echo str_replace('<valid />', '<p>'.$result.'</p>', $pageHTML, );
 ?>
