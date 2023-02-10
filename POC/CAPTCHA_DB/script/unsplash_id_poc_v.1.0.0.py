0c29f26ab4ec89faa1c7ec75015e53972dbe9c0b
Matteo Stocco <mstocco11@gmail.com>
2023-02-11 00:52:54 +0100

    populated the database
    CEA-67 #time 1h 45m

--- POC/CAPTCHA_DB/script/unsplash_id_poc_v.1.0.0.py
+++ POC/CAPTCHA_DB/script/unsplash_id_poc_v.1.0.0.py
@@ -4,31 +4,33 @@ import os
 import mysql.connector
 from time import sleep
 import cv2 as cv
-import urllib.request
-import numpy as np
+import pymysql
 
 
 ACCESS_KEY = "1CoGBz63unkif-f4iBuOzL4g6o2oP-OMmFoMs7quj24"
-valuableWord = ["ship", "car", "bird", "bottle", "laptop"]
-db = mysql.connector.connect(
-    host ="localhost",
-    user ="root",
-    passwd = "root-1234",
-    database = "captcha_db"
-)
+valuableWord = ["car"]
+classi = ["macchine"]
+# database connection
+db = pymysql.connect(host="localhost", port=3307, user="root", passwd="root", database="captcha_db")
 mycursor = db.cursor()
-for i in range(5):
+
+for i in range(1):
     r = rq.get(
           'https://api.unsplash.com/photos/random/?w=200&h=200&query={0}&client_id={1}'.format(valuableWord[i], ACCESS_KEY),allow_redirects=True)
     jsonContent = json.loads(r.content)
-    mycursor.execute("INSERT INTO immagine (idImmagine,classeImmagine,sicurezzaImmagine) VALUES(%s,%s,%s)", (jsonContent["id"],valuableWord[i],0))
+    dir = "./assets/" + jsonContent["id"] + '.jpg'
+    print(jsonContent["id"])
+    mycursor.execute("INSERT INTO image (id,class,reliability,path) VALUES(%s,%s,%s,%s)", (jsonContent["id"],classi[i],50,dir))
     db.commit()
-    dir = 'C:/Users/mstoc/catchEmAll-Docs/POC/CAPTCHA_DB/assets/' + jsonContent["id"] + '.jpg'
     image = rq.get(jsonContent["urls"]["regular"], allow_redirects=True)
-    open(dir, 'wb').write(image.content)
+    with open(dir, 'wb') as f:
+        f.write(image.content)
     raw_image = cv.imread(dir)
     gray = cv.cvtColor(raw_image, cv.COLOR_BGR2GRAY)
     bilFilter = cv.bilateralFilter(gray, 11, 17, 17)
     canny = cv.Canny(bilFilter, 20, 200)
     cv.imwrite(dir, canny)
+
+
+
 
